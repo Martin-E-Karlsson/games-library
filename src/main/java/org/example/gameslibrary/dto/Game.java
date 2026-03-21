04bdf4f5cbfa6975532aa3420b2cef8edf1e0650
Martin Karlsson <m.karlsson91@gmail.com>
2026-03-22 00:59:19 +0100

    Initial commit after project creation. Added directories for controller, entity and dto

--- src/main/java/org/example/gameslibrary/dto/Game.java
+++ src/main/java/org/example/gameslibrary/dto/Game.java
@@ -1,6 +1,12 @@
 package org.example.gameslibrary.dto;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 public class Game {
@@ -10,11 +16,70 @@ public class Game {
     @Column(nullable = false)
     private Long id;
 
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String description;
+
+    @PastOrPresent
+    private LocalDate releaseDate;
+
+    @NotBlank
+    private BigDecimal price;
+
+    @NotBlank
+    private List<String> categories;
+
+    public Game() {
+        // Constructor required by JPA
+    }
+
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public List<String> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<String> categories) {
+        this.categories = categories;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 }
